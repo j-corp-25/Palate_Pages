a0d137fe5137374c7506f9b0ad49981206123237
Jordy <jordycorporan95@gmail.com>
2024-01-18 01:09:44 -0500

    customize tailwind

--- palatepages-frontend/tailwind.config.js
+++ palatepages-frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, //
+  darkMode: 'class', //
   theme: {
     extend: {},
   },
