c5ae4dd382d30104e384f698c68ed118fd59370b
Jordy <jordycorporan95@gmail.com>
2024-01-21 16:57:14 -0500

    format images

--- palatepages-frontend/components/RecipeItem.jsx
+++ palatepages-frontend/components/RecipeItem.jsx
@@ -4,18 +4,31 @@ import Image from 'next/image'
 
 const RecipeItem = ({ recipe }) => {
   return (
-    <div>
-      <div>
+    <div className='flex border-b border-gray-300 py-4'>
+      <div className='mr-4 w-44 h-44 relative'>
         <Image
           src={recipe.image ? recipe.image : '/test/default-image.png'}
-          width={170}
-          height={100}
+          layout='fill'
+          objectFit='cover'
+          className='rounded'
+          alt={recipe.name}
         />
       </div>
-      <div>
-        <h3> Description: {recipe.description}</h3>
-        <h3>Chef: {recipe.chef}</h3>
-        <h3>Name: {recipe.name}</h3>
+      <div className='flex flex-col justify-center'>
+        <h3 className='text-2xl font-semibold mb-3 text-light-text'>
+          {recipe.name}
+        </h3>
+        <p className='text-light-text mb-4'>{recipe.chef}</p>
+        <p className='text-light-text text-sm'>{recipe.description}</p>
+        <div className='mt-5'>
+          <Link href={`/recipes/${recipe.slug}`}>
+            <div
+              className='inline-block bg-main-color hover:text-dark-text text-light-text py-2 px-4 rounded'
+            >
+              Details
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
   )
